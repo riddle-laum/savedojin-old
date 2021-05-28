4e010e66ab3ed3945a4deaf059c26be4f730c709
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-05-28 10:01:48 +0900

    Add files via upload

---
+++ v6_local/next/main.js
@@ -0,0 +1,30 @@
+// namespace
+const SaveDojin = class{
+  // version
+  static get version(){return 'v??? extends savedojin6.0.0(alpha)'};
+
+  // support list
+  static get support(){return Object.keys()} // 悩み
+
+  // main
+  static async exec(){};
+
+  // imglist dom maker
+  static async imglist({urls, title}){};
+
+  // module
+  _module = {};
+
+  // module functions
+  _functions = {};
+
+  // assets
+  _assets = {};
+};
+
+// set modules & functions
+
+// set assets
+
+// ----- onload ----- //
+SaveDojin.exec();
